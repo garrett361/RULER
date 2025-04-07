572f07920ba0d14baefe35503606b1e4b0818b7b
Garrett Goon <garrett361@gmail.com>
2025-04-07 14:31:36 +0000

    configurable batch size

--- scripts/run.sh
+++ scripts/run.sh
@@ -27,7 +27,7 @@ GPUS="8" # GPU size for tensor_parallel.
 ROOT_DIR="benchmark_root" # the path that stores generated task samples and model predictions.
 MODEL_DIR=${MODEL_DIR} # the path that contains individual model folders from HUggingface.
 ENGINE_DIR="." # the path that contains individual engine folders from TensorRT-LLM.
-BATCH_SIZE=1  # increase to improve GPU utilization
+BATCH_SIZE=${BATCH_SIZE:-1}  # increase to improve GPU utilization
 
 
 # Model and Tokenizer
