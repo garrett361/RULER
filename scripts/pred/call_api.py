bcb6e6397b3a8d94b7f1aea227b598bb7225e6cf
Garrett Goon <goon@ibm.com>
2025-09-01 13:52:08 -0400

    sleep after fail in call_api.py

--- scripts/pred/call_api.py
+++ scripts/pred/call_api.py
@@ -21,7 +21,7 @@ dataset jsonl:
     "outputs": [str],
 }
 
-prediction jsonl: 
+prediction jsonl:
 {
     "index" int,
     "input": str,
@@ -76,7 +76,7 @@ parser.add_argument("--server_host", type=str, default='127.0.0.1')
 parser.add_argument("--server_port", type=str, default='5000')
 parser.add_argument("--ssh_server", type=str)
 parser.add_argument("--ssh_key_path", type=str)
-parser.add_argument("--model_name_or_path", type=str, default='gpt-3.5-turbo', 
+parser.add_argument("--model_name_or_path", type=str, default='gpt-3.5-turbo',
                     help='supported models from OpenAI or HF (provide a key or a local path to the checkpoint)')
 
 # Inference
@@ -141,7 +141,7 @@ def get_llm(tokens_to_generate):
             stop=args.stop_words,
             tokens_to_generate=tokens_to_generate,
         )
-        
+
     elif args.server_type == 'openai':
         from client_wrappers import OpenAIClient
         llm = OpenAIClient(
@@ -165,7 +165,7 @@ def get_llm(tokens_to_generate):
             stop=args.stop_words,
             tokens_to_generate=tokens_to_generate,
         )
-        
+
     elif args.server_type == 'hf':
         from model_wrappers import HuggingFaceModel
         llm = HuggingFaceModel(
@@ -178,7 +178,7 @@ def get_llm(tokens_to_generate):
             stop=args.stop_words,
             max_new_tokens=tokens_to_generate,
         )
-    
+
     elif args.server_type == 'mamba':
         from model_wrappers import MambaModel
         # mamba uses its own generation function, do not pass in do_sample
@@ -192,7 +192,7 @@ def get_llm(tokens_to_generate):
             stop=args.stop_words,
             max_new_tokens=tokens_to_generate,
         )
-        
+
     else:
         raise RuntimeError(f'Unsupported server type {args.server_type}')
 
@@ -201,9 +201,9 @@ def get_llm(tokens_to_generate):
 
 def main():
     start_time = time.time()
-    
+
     curr_folder = os.path.dirname(os.path.abspath(__file__))
-    
+
     try:
         sys.path.append(os.path.dirname(curr_folder))
         module = importlib.import_module(f"data.{args.benchmark}.constants")
@@ -216,17 +216,17 @@ def main():
 
     if args.task not in tasks_customized:
         raise ValueError(f'{args.task} is not found in config_tasks.yaml')
-        
+
     config = tasks_customized.get(args.task)
     config.update(tasks_base[config['task']])
 
     task_file = args.data_dir / args.task / f'{args.subset}.jsonl'
-    
+
     if args.chunk_amount > 1:
         pred_file = args.save_dir / f'{args.task}-{args.chunk_idx}.jsonl'
     else:
         pred_file = args.save_dir / f'{args.task}.jsonl'
-        
+
     print(f'Predict {args.task} \nfrom {task_file}\nto {pred_file}')
     pred_file.parent.mkdir(parents=True, exist_ok=True)
 
@@ -249,6 +249,7 @@ def main():
                 break
             except Exception as e:
                 traceback.print_exc()
+                time.sleep(os.getenv("RULER_SLEEP_SEC", 30))
 
         zipped_iter = zip(pred_list, idx_list, index_list, input_list,
                           outputs_list, others_list, truncation_list, length_list)
